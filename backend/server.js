67251181a6f1fcd36b83a2febb74fc7ac8bbfa00
Moshe-Ur Rahman <mosheeurrahman76@gmail.com>
2026-03-28 14:56:26 +0600

    prepare backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true}));
 app.use(express.json());
 
 // Routes
